db923ee1d854142f3a71bf193f3cb29aaa962440
drmaluta <admin@bardsong.com>
2018-02-15 16:23:54 -0800

    COMPLETED all TODO tasks

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,20 @@
+package com.example.android.sunshine.sync;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
+
 // TODO (9) Create a class called SunshineSyncUtils
+public class SunshineSyncUtils {
+
     //  TODO (10) Create a public static void method called startImmediateSync
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+    public static void startImmediateSync (@NonNull final Context context) {
+        //  TODO (11) Within that method, start the SunshineSyncIntentService
+        Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(intentToSyncImmediately);
+    }
+}
